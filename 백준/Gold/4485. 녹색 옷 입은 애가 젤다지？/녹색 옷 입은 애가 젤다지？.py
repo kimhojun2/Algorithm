c163c719fe7e43ccd4f294775f1cda4374f6e2ec
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2024-03-19 13:13:13 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 96 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -0,0 +1,43 @@
+import heapq
+import sys
+
+f = sys.stdin.readline
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dijkstra():
+    q = []
+    heapq.heappush(q, (graph[0][0], 0, 0))
+    distance[0][0] = 0
+
+    while q:
+        cost, x, y = heapq.heappop(q)
+
+        if x == n - 1 and y == n - 1:
+            print(f'Problem {count}: {distance[x][y]}')
+            break
+
+        for i in range(4):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+
+            if 0 <= new_x < n and 0 <= new_y < n:
+                new_cost = cost + graph[new_x][new_y]
+
+                if new_cost < distance[new_x][new_y]:
+                    distance[new_x][new_y] = new_cost
+                    heapq.heappush(q, (new_cost, new_x, new_y))
+
+count = 1
+
+while True:
+    n = int(f())
+    if n == 0:
+        break
+
+    graph = [list(map(int, f().split())) for _ in range(n)]
+    distance = [[float('inf')] * n for _ in range(n)]
+
+    dijkstra()
+    count += 1
