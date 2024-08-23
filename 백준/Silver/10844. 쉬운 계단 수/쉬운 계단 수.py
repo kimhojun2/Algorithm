2813a413a26ca0a38568c655f19b4d05ba4e7a0f
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2024-08-23 23:52:35 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -6,12 +6,12 @@ dp[0] = [0,1,1,1,1,1,1,1,1,1]
 for i in range(1, N):
     for j in range(10):
         if j == 0:
-            dp[i][0] = dp[i-1][1]
+            dp[i][0] = dp[i-1][1] % 1000000000
 
         elif j == 9:
-            dp[i][9] = dp[i-1][8]
+            dp[i][9] = dp[i-1][8] % 1000000000
 
         else:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+            dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000
 
-print(sum(dp[N-1])%1000000000)
+print(sum(dp[N-1]) % 1000000000)
