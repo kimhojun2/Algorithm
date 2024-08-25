d3fd479a3f85c9c3cbbbe240fa3157bbc10749cf
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2024-08-26 00:13:38 +0900

    [Silver V] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,18 @@
+N = int(input())
+ans = 0
+while True:
+    if N%5 == 0:
+        ans += N//5
+        break
+
+    else:
+        N -= 2
+        ans += 1
+
+    if N < 0:
+        break
+
+if N < 0:
+    print(-1)
+else:
+    print(ans)
