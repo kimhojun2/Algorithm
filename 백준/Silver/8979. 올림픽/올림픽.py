5ba4afdc72011e5adc3a626924f35e1e059a073b
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2024-09-14 14:39:47 +0900

    [Silver V] Title: 올림픽, Time: 56 ms, Memory: 34516 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -5,15 +5,14 @@ import heapq
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-gold = {n:0 for n in range(1,N+1)}
-silver = {n:0 for n in range(1,N+1)}
-bronze = {n:0 for n in range(1,N+1)}
+gold = {n: 0 for n in range(1, N + 1)}
+silver = {n: 0 for n in range(1, N + 1)}
+bronze = {n: 0 for n in range(1, N + 1)}
 ranking = []
-
 
 for n in range(N):
     nation, g, s, b = map(int, input().split())
-    heapq.heappush(ranking,(-1*g,-1*s,-1*b, nation))
+    heapq.heappush(ranking, (-1 * g, -1 * s, -1 * b, nation))
 
 now_g = -1
 now_s = -1
@@ -21,23 +20,19 @@ now_b = -1
 rank = 0
 same = 0
 while ranking:
-    ng,ns,nb,nn = heapq.heappop(ranking)
-    Ng = -1* ng
+    ng, ns, nb, nn = heapq.heappop(ranking)
+    Ng = -1 * ng
     Ns = -1 * ns
     Nb = -1 * nb
 
-    if now_g == Ng and now_s == Ns and npw_b == Nb:
+    if now_g == Ng and now_s == Ns and now_b == Nb:
         same += 1
-
     else:
-        if same:
-            rank += same
-            same = 0
-        else:
-            rank += 1
+        rank += same + 1
+        same = 0
         now_g = Ng
         now_s = Ns
-        npw_b = Nb
+        now_b = Nb
 
     if nn == K:
         print(rank)
