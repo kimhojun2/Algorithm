e3f5adcd22ebc5018d141840432413f1d6971595
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2024-12-30 23:55:01 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+
+    char arr[5][16] = {0};
+
+    for (int i = 0;i < 5;i++) {
+        scanf("%s", arr[i]);
+    }
+
+    for (int i = 0;i < 15;i++) {
+        for (int j = 0;j < 5;j++) {
+            if (arr[j][i] != NULL) {
+                printf("%c", arr[j][i]);
+            }
+        }
+    }
+
+    return 0;
+}
