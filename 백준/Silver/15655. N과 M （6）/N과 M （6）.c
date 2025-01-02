cb9e780e7302d0b9cb2859008929f6c790c5c266
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-01-02 12:06:37 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int N = 0, M = 0;
+int arr[9] = { 0, };
+int visited[9] = { 0, };
+int ans[9] = { 0, };
+
+void back(int level, int max);
+
+int compare(const void* a, const void* b) {
+	int num1 = *(int*)a;
+	int num2 = *(int*)b;
+
+	if (num1 < num2) {
+		return -1;
+	}
+	else if (num1 > num2) {
+		return 1;
+	}
+
+	return 0;
+}
+
+
+int main() {
+
+	int i = 0;
+	scanf("%d %d", &N, &M);
+	while (i < N) {
+		scanf("%d", &arr[i]);
+		i++;
+	}
+	qsort(arr, N, sizeof(int), compare);
+
+	back(0,0);
+
+	return 0;
+}
+
+void back(int level, int max) {
+	if (level == M) {
+		for (int i = 0;i < level;i++) {
+			printf("%d ", ans[i]);
+		}
+		printf("\n");
+	}
+	else {
+		for (int i = 0;i < N;i++) {
+			if (visited[i]==0 && arr[i]>max) {
+				ans[level] = arr[i];
+				visited[i] = 1;
+				back(level + 1,arr[i]);
+				visited[i] = 0;
+			}
+		}
+	}
+}
