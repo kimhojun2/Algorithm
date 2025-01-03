407a1b4b9c08cbf0f9654a1072e409f77dbc934d
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-01-03 17:00:01 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,29 +1,22 @@
 N = int(input())
 arr = list(map(int, input().split()))
 
-def sol(arr):
+def LIS():
     dp = [1] * N
-    prev = [-1] * N  # 이전 인덱스를 추적할 배열
-
-    for i in range(1, N):
+    for i in range(N):
         for j in range(i):
             if arr[j] < arr[i] and dp[i] < dp[j] + 1:
                 dp[i] = dp[j] + 1
-                prev[i] = j  # 이전 인덱스를 기록
 
-    # LIS 길이와 끝나는 인덱스를 찾음
-    max_len = max(dp)
-    idx = dp.index(max_len)
 
-    # LIS 재구성
-    lis = []
-    while idx != -1:
-        lis.append(arr[idx])
-        idx = prev[idx]
+    L = max(dp)
+    print(L)
+    ans = []
+    for idx in range(N-1,-1,-1):
+        if dp[idx] == L:
+            ans.append(arr[idx])
+            L -= 1
+    ans.reverse()
+    print(*ans)
 
-    lis.reverse()  # 역순이므로 뒤집기
-    return max_len, lis
-
-length, sequence = sol(arr)
-print(length)  # LIS의 길이
-print(*sequence)  # LIS
+LIS()
