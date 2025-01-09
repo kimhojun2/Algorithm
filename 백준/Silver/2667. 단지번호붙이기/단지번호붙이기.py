9f99bdd00bad8d22a04e7f911af3a831593b1d4b
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-01-09 12:43:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,30 @@
-import sys
-from collections import deque
 N = int(input())
 
-arr = [list(input()) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
+arr = [list(map(int, input())) for _ in range(N)]
+
 di = [0,0,1,-1]
 dj = [1,-1,0,0]
 
+def dfs(i,j):
+    global cnt
+    arr[i][j] = 0
+    cnt += 1
+    for d in range(4):
+        ni, nj = i +di[d], j+dj[d]
+        if 0<=ni<N and 0<=nj<N:
+            if arr[ni][nj] == 1:
+                dfs(ni,nj)
 
-def bfs(y,x):
-    q = deque()
-    q.append((y,x))
-    visited[y][x] = 1
-    arr[y][x] = '0'
-    cnt = 1
-    while q:
-        i, j = q.popleft()
-        for d in range(4):
-            ni, nj = i + di[d], j + dj[d]
-            if 0<= ni <N and 0<= nj < N and visited[ni][nj] == 0 and arr[ni][nj] == '1':
-                arr[ni][nj] = '0'
-                visited[ni][nj] = 1
-                q.append((ni,nj))
-                cnt += 1
 
-    return cnt
-
-ans = 0
-num = []
+ans = []
 for y in range(N):
     for x in range(N):
-        if arr[y][x] == '1':
-            ans += 1
-            num.append(bfs(y,x))
+        if arr[y][x] == 1:
+            cnt = 0
+            dfs(y,x)
+            ans.append(cnt)
 
-print(ans)
-num.sort()
-for n in num:
-    print(n)
+ans.sort()
+print(len(ans))
+for a in ans:
+    print(a)
