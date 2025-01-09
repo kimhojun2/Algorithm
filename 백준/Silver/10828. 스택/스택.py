f67bfbcb845cd9f526bb62af4ea7b67195ec51d6
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-01-09 20:15:43 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,44 +1,34 @@
-# 10828 스택
-
-'''
-push X: 정수 X를 스택에 넣는 연산이다.
-pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-size: 스택에 들어있는 정수의 개수를 출력한다.
-empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
-top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-'''
-
-from collections import deque
 import sys
 
-N = int(sys.stdin.readline())
-dq = deque()
-len = 0
+input = sys.stdin.readline
+
+N = int(input())
+
+stack = []
+
 for i in range(N):
-    order = sys.stdin.readline().split()
+    order = list(map(str, input().split()))
 
     if order[0] == 'push':
-        dq.append(order[1])
-        len += 1
+        stack.append(order[1])
 
-    elif order[0] == 'pop':
-        if len != 0:
-            print(dq.pop())
-            len -= 1
+    elif order[0] == "pop":
+        if stack:
+            print(stack.pop())
         else:
             print(-1)
 
-    elif order[0] == 'size':
-        print(len)
+    elif order[0] == "size":
+        print(len(stack))
 
-    elif order[0] == 'empty':
-        if len != 0:
+    elif order[0] == "empty":
+        if stack:
             print(0)
         else:
             print(1)
 
     else:
-        if len != 0:
-            print(dq[-1])
+        if stack:
+            print(stack[-1])
         else:
             print(-1)
