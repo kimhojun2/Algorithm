9f8f269bc8e382ca794a0c76845c023d64515a8a
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-03-03 15:56:07 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -3,27 +3,25 @@
 #include <algorithm>
 using namespace std;
 
-int N, K;
-
-int dp[100001] = { 0, };
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-	cin >> N >> K;
+    int N, K;
+    cin >> N >> K;
+    vector<int> dp(K + 1, 0);
 
-	for (int i = 0;i < N;i++) {
-		int W, V;
-		cin >> W >> V;
-		if (W > K) {
-			continue;
-		}
-		for (int j = K;j >= W ;j--) {
-			dp[j] = max(dp[j], dp[j - W] + V);
-		}
-	}
-	cout << dp[K];
+    for (int i = 0; i < N; i++) {
+        int W, V;
+        cin >> W >> V;
 
-	return 0;
+        if (W > K) continue;
+
+        for (int i = K; i >= W; i--) {
+            dp[i] = max(dp[i], dp[i - W] + V);
+        }
+    }
+
+    cout << dp[K];
+    return 0;
 }
