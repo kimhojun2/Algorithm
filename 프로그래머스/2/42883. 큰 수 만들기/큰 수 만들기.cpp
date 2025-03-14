168cf7dce10cf8985e859682f51add80aafa0f90
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-03-14 10:39:43 +0900

    Update 큰 수 만들기.cpp

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,25 +1,34 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <map>
-#include <algorithm>
 using namespace std;
 
-
 string solution(string number, int k) {
-    string result = "";
-    int n = number.size();
-    int to_remove = k; // 제거해야 할 개수
-
-    for (char c : number) {
-        while (!result.empty() && to_remove > 0 && result.back() < c) {
-            result.pop_back();
-            to_remove--;
+    string answer = "";
+    
+    // answersize만큼 for문을 돌려서 한 번에 한 글자씩 answer를 따오고, 한 번 돌릴 때마다 startidx부터 k+i까지 돌린다.
+    // 이렇게 한도를 두고 돌려야 answersize만큼의 answer 글자들을 뽑아올 수 있기 때문이다.
+    
+    int answersize = number.size() - k; // return값의 길이는 number의 사이즈에서 k를 뺀 것
+    int startidx = 0; // 시작 index
+    
+    for(int i = 0; i < answersize; ++i){ // answersize만큼 반복
+        
+        char maxint = number[startidx]; // 처음에는 startidx의 값이 최대값
+        int maxidx = startidx; // 처음에는 maxidx가 startidx
+        
+        for(int j = startidx; j <= k+i; j++){ // startidx부터 k+i까지 확인해서 max값 찾음 -k
+            
+            if(maxint < number[j]){ // 더 큰 값이 나오면 그 값의 index와 number를 저장
+                maxint = number[j];
+                maxidx = j;
+            }
         }
-        result.push_back(c);
+        
+        startidx = maxidx + 1; // 다음번에는 maxindex + 1에서부터 시작
+        answer += maxint; // answer에 가장 큰 수를 넣는다
+        cout << answer<<'\n';
     }
-
-    // 아직 k개를 다 제거하지 못했다면 뒤에서 자르기
-    result = result.substr(0, n - k);
-    return result;
+    
+    return answer;
 }
