eae367c2c2dcad721935166585fb2f6a95faa3c1
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-03-15 19:24:54 +0900

    [Gold III] Title: 소문난 칠공주, Time: 324 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -8,79 +8,87 @@ int dy[] = { 0,0,1,-1 };
 int dx[] = { 1,-1,0,0 };
 int answer = 0;
 
-bool isConnected(vector<pair<int, int>>& lst) {
-    queue<pair<int, int>> q;
+
+bool check_connect(vector<pair<int, int>>& lst) {
+    queue<pair<int, int>>q;
     bool visited[5][5] = { false, };
 
     q.push(lst[0]);
     int check_cnt = 0;
 
-    for (auto& now : lst) {
+    for (pair<int, int>& now : lst) {
         visited[now.first][now.second] = true;
     }
     visited[lst[0].first][lst[0].second] = false;
+
     while (!q.empty()) {
+
         check_cnt++;
-        
+
         if (check_cnt == 7) return true;
-        
+
         int y = q.front().first;
         int x = q.front().second;
         q.pop();
 
-        for (int d = 0; d < 4; d++) {
+        for (int d = 0;d < 4;d++) {
             int ny = y + dy[d];
             int nx = x + dx[d];
-
             if (ny < 0 || nx < 0 || ny >= 5 || nx >= 5) continue;
             if (!visited[ny][nx]) continue;
 
-            q.push({ ny, nx });
+            q.push({ ny,nx });
             visited[ny][nx] = false;
+
         }
+
     }
     return false;
 }
 
+
 void make(int idx, int level, vector<pair<int, int>>& lst, int S_cnt) {
-    if (level == 7) {
-         if (S_cnt >= 4 && isConnected(lst)) {
+    if (level == 7 && check_connect(lst)) {
+        if (S_cnt >= 4) {
             answer++;
-         }
-        return;
+            return;
+        }
     }
 
-    for (int i = idx; i < 25; i++) {
+    for (int i = idx;i < 25;i++) {
         int y = i / 5;
         int x = i % 5;
 
-        lst.push_back({ y, x });
+        lst.push_back({ y,x });
 
         if (arr[y][x] == 'S') {
-            make(i+1, level + 1, lst, S_cnt + 1);
+            make(i + 1, level + 1, lst, S_cnt+1);
         }
         else {
-            make(i+1, level + 1, lst, S_cnt);
+            make(i + 1, level + 1, lst, S_cnt);
         }
 
         lst.pop_back();
     }
+    
 }
 
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0); cout.tie(0);
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 5;i++) {
+        for (int j = 0;j < 5;j++) {
             cin >> arr[i][j];
         }
     }
 
     vector<pair<int, int>> lst;
+
     make(0, 0, lst, 0);
 
     cout << answer;
+
     return 0;
 }
