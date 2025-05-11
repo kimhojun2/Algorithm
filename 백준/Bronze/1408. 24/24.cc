02b3187ae9e9b42a779faaa5827073c4dbd833ed
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-05-11 23:35:33 +0900

    [Bronze II] Title: 24, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/1408. 24/24.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int gettime(string t){
+    int h = (t[0]-'0') * 10 + t[1]-'0';
+    int m = (t[3]-'0') * 10 + t[4]-'0';
+    int s = (t[6]-'0') * 10 + t[7]-'0';
+    return h*3600+m*60+s;
+}
+
+string inversetime(int t){
+    string ret, temp;
+    temp = to_string(t%60); if(temp.length()==1) temp='0'+temp;
+    ret = ':'+temp;
+    t /= 60;
+    temp = to_string(t%60); if(temp.length()==1) temp='0'+temp;
+    ret = ':'+temp+ret;
+    t /= 60;
+    temp = to_string(t%60); if(temp.length()==1) temp='0'+temp;
+    ret = temp+ret;
+    return ret;
+}
+
+int main(){
+    string st, en; cin >> st >> en;
+    int st_ = gettime(st);
+    int en_ = gettime(en);
+    int v = en_ - st_;
+    if(en_ < st_) v = 3600*24 - (st_-en_);
+    auto res = inversetime(v);
+    cout << res;
+    
+    return 0;
+}
