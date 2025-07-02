5300a545b968051348af7f8ff77721abe704deb8
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-02 19:50:53 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,29 +1,27 @@
-def dfs(position):
-    global result
-    x, y, dir = position
-    if x == n - 1 and y == n - 1:
-        result += 1
-        return
-    if x + 1 < n and y + 1 < n: # 대각선 이동
-        if graph[x + 1][y + 1] == 0 and graph[x][y + 1] == 0 and graph[x + 1][y] == 0:
-            dfs((x + 1, y + 1, 2))
-    if dir == 0 or dir == 2: # 세로 이동
-        if y + 1 < n:
-            if graph[x][y + 1] == 0:
-                dfs((x, y + 1, 0))
-    if dir == 1 or dir == 2: # 가로 이동
-        if x + 1 < n:
-            if graph[x + 1][y] == 0:
-                dfs((x + 1, y, 1))
+import sys
+input = sys.stdin.readline
 
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0]*3 for _ in range(N)] for _ in range(N)]
 
-n = int(input())
-graph = [[] for _ in range(n)]
-result = 0
+dp[0][1][0] = 1
 
-for i in range(n):
-    graph[i] = list(map(int, input().split()))
+for y in range(N):
+    for x in range(2, N):
+        if arr[y][x] == 1:
+            continue
 
-dfs((0, 1, 0))
+        # 가로
+        dp[y][x][0] = dp[y][x-1][0] + dp[y][x-1][2] if arr[y][x-1] == 0 else 0
 
-print(result)
+        # 세로
+        if y > 0:
+            dp[y][x][1] = dp[y-1][x][1] + dp[y-1][x][2] if arr[y-1][x] == 0 else 0
+
+        # 대각선
+        if y > 0 and arr[y-1][x] == 0 and arr[y][x-1] == 0 and arr[y-1][x-1] == 0:
+            dp[y][x][2] = dp[y-1][x-1][0] + dp[y-1][x-1][1] + dp[y-1][x-1][2]
+
+# 정답: (N-1,N-1)에 도달하는 모든 방향의 경로 수 합
+print(sum(dp[N-1][N-1]))
