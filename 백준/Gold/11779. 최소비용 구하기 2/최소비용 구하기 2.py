cc5f63e7ee4f9331ae2cc8121d58f3a6996cc295
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-04 10:48:27 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 2744 ms, Memory: 41408 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -1,47 +1,39 @@
-import sys
+from collections import defaultdict
 import heapq
-input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
 
-dic = {}
-for _ in range(M):
-    A, B, C = map(int, input().split())
-    if A not in dic:
-        dic[A] = {B: C}
-    else:
-        if B in dic[A]:
-            dic[A][B] = min(dic[A][B], C)
-        else:
-            dic[A][B] = C
+graph = defaultdict(dict)
+
+for i in range(M):
+    s, e, v = map(int, input().split())
+    if e not in graph[s] or graph[s][e] > v:
+        graph[s][e] = v
 
 start, end = map(int, input().split())
 
-def dijkstra(start, end):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance = [[float('inf'), []] for _ in range(N + 1)]
-    distance[start][0] = 0
-    distance[start][1] = [start]
+distance = [[float('INF'), []] for _ in range(N+1)]
+hq = [(0, [start])]
+distance[start][0] = 0
+distance[start][1] = [start]
 
-    while q:
-        dist, now = heapq.heappop(q)
+while hq:
+    dist, path = heapq.heappop(hq)
+    now = path[-1]
 
-        if dist > distance[now][0]:
-            continue
+    if distance[now][0] < dist:
+        continue
 
-        for next_node in dic.get(now, {}):
-            cost = dist + dic[now][next_node]
+    for next, d in graph[now].items():
+        cost = dist + d
+        if distance[next][0] > cost:
+            new_path = path + [next]
+            distance[next][0] = cost
+            distance[next][1] = new_path
+            heapq.heappush(hq, (cost, new_path))
 
-            if cost < distance[next_node][0]:
-                distance[next_node][0] = cost
-                distance[next_node][1] = distance[now][1] + [next_node]
-                heapq.heappush(q, (cost, next_node))
-
-    return distance[end]
-
-min_cost, arr = dijkstra(start, end)
-print(min_cost)
-print(len(arr))
-print(*arr)
+# 최단 거리 및 경로 출력 예시
+print(distance[end][0])
+print(len(distance[end][1]))
+print(*distance[end][1])
