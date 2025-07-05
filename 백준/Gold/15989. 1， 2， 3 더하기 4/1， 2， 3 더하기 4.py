36de5252b75ac5d0bf84633ee5d8133d09f880ef
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-05 14:13:44 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 68 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -1,14 +1,20 @@
 T = int(input())
 
+dp = [[0]*3 for _ in range(10001)]
+
+dp[1][0] = 1
+dp[2][0] = 1
+dp[2][1] = 1
+dp[3][0] = 1
+dp[3][1] = 1
+dp[3][2] = 1
+
+for i in range(4, 10001):
+    dp[i][0] = dp[i-1][0]
+    dp[i][1] = dp[i-2][1] + dp[i-2][0]
+    dp[i][2] = dp[i-3][2] + dp[i-3][1] + dp[i-3][0]
+
+
 for tc in range(T):
-    N = int(input())
-
-
-    cnt = 0
-    K = N
-    while K > 3:
-        cnt += 1
-        cnt += (K // 2)
-        K -= 3
-
-    print(cnt + K)
+    n = int(input())
+    print(sum(dp[n]))
