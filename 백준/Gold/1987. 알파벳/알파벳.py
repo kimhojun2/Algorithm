3dd4f8e871e23a06b0a56babd9fcf5fe87676764
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-05 16:34:26 +0900

    [Gold IV] Title: 알파벳, Time: 4648 ms, Memory: 164484 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,33 +1,31 @@
-# 백준 알파벳(1987)
-import sys
-input = sys.stdin.readline
-
-R, C = map(int, input().split())
-arr = [list(input().strip()) for _ in range(R)]
-
-di = [0,0,1,-1]
-dj = [1,-1,0,0]
-check = [0] * 26
-visited = [[0] * C for _ in range(R)]
-visited[0][0] = 1
-start = arr[0][0]
-check[ord(start)-65] = 1
-cnt = 1
-
-def dfs(y, x, total):
-    global cnt
-    cnt = max(cnt, total)
+def backtracking(y,x, cnt):
+    global ans
     for d in range(4):
-        ny, nx = y + di[d], x + dj[d]
-        if 0<= ny < R and 0 <= nx < C and not visited[ny][nx]:
-            now = arr[ny][nx]
-            num = ord(now) - 65
-            if not check[num]:
-                visited[ny][nx] = 1
-                check[num] = 1
-                dfs(ny, nx, total + 1)
-                check[num] = 0
-                visited[ny][nx] = 0
+        ny, nx = y + dy[d], x + dx[d]
+        if ny < 0 or nx < 0 or ny >= N or nx >= M: continue
+        now = ord(graph[ny][nx]) - 65
+        if checklist[now] == 1: continue
+        checklist[now] = 1
+        backtracking(ny,nx, cnt+1)
+        checklist[now] = 0
 
-dfs(0, 0, 1)
-print(cnt)
+    ans = max(ans, cnt)
+
+    return
+
+
+N, M = map(int, input().split())
+
+graph = [list(input()) for _ in range(N)]
+visited = [[0]*M for _ in range(N)]
+checklist = [0]*26
+visited[0][0] = 1
+checklist[ord(graph[0][0])-65] = 1
+
+dy = [0,0,1,-1]
+dx = [1,-1,0,0]
+ans = 1
+
+backtracking(0,0,1)
+
+print(ans)
