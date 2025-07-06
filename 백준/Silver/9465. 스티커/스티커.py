6c05c771a2b20f5e741c8aa547d43737466d3df5
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-06 11:57:14 +0900

    [Silver I] Title: 스티커, Time: 948 ms, Memory: 63520 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,27 +1,13 @@
 T = int(input())
 
 for tc in range(T):
-    arr = []
-    N = int(input())
-    for _ in range(2):
-        row = list(map(int, input().split()))
-        arr.append(row)
-    dp = [[0,0] for _ in range(N)]
-    if N == 1:
-        print(max(arr[0][0], arr[1][0]))
-        continue
+    n = int(input())
+    graph = [list(map(int, input().split())) for _ in range(2)]
+    # [안뽑, 위, 아래]
+    dp = [[0,0,0] for _ in range(n)]
+    dp[0] = [0,graph[0][0], graph[1][0]]
 
-    for i in range(N):
-        if i == 0:
-            dp[0][0] = arr[0][0]
-            dp[0][1] = arr[1][0]
+    for i in range(1,n):
+        dp[i] = [max(dp[i-1]),graph[0][i] + max(dp[i-1][0], dp[i-1][2]), graph[1][i] + max(dp[i-1][0],dp[i-1][1])]
 
-        elif i == 1:
-            dp[1][0] = dp[0][1] + arr[0][1]
-            dp[1][1] = dp[0][0] + arr[1][1]
-
-        else:
-            dp[i][0] = max(max(dp[i-2]) + arr[0][i], dp[i-1][1] + arr[0][i])
-            dp[i][1] = max(max(dp[i-2]) + arr[1][i], dp[i-1][0] + arr[1][i])
-
-    print(max(dp[N-1]))
+    print(max(dp[n-1]))
