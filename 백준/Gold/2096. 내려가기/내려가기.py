b9c90e5bd3caad33dc8974cd5dd2f10d0aa8c509
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-06 21:48:26 +0900

    [Gold V] Title: 내려가기, Time: 172 ms, Memory: 122132 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -2,27 +2,22 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
 
-a, b, c = map(int, input().split())
-max_dp = [a, b, c]
-min_dp = [a, b, c]
+max_dp = graph[0][:]
+min_dp = graph[0][:]
 
-for _ in range(1, N):
-    a, b, c = map(int, input().split())
+for i in range(1, N):
+    a, b, c = graph[i]
+    max0 = max(max_dp[0], max_dp[1]) + a
+    max1 = max(max_dp[0], max_dp[1], max_dp[2]) + b
+    max2 = max(max_dp[1], max_dp[2]) + c
 
-    new_max_dp = [
-        max(max_dp[0], max_dp[1]) + a,
-        max(max_dp[0], max_dp[1], max_dp[2]) + b,
-        max(max_dp[1], max_dp[2]) + c
-    ]
+    min0 = min(min_dp[0], min_dp[1]) + a
+    min1 = min(min_dp[0], min_dp[1], min_dp[2]) + b
+    min2 = min(min_dp[1], min_dp[2]) + c
 
-    new_min_dp = [
-        min(min_dp[0], min_dp[1]) + a,
-        min(min_dp[0], min_dp[1], min_dp[2]) + b,
-        min(min_dp[1], min_dp[2]) + c
-    ]
-
-    max_dp = new_max_dp
-    min_dp = new_min_dp
+    max_dp = [max0, max1, max2]
+    min_dp = [min0, min1, min2]
 
 print(max(max_dp), min(min_dp))
