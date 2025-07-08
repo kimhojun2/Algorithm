94f9fc695dedee778b4011bb8f70e0afa075a06a
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-08 21:47:56 +0900

    [Gold IV] Title: 연구소, Time: 1460 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,57 +1,44 @@
 import sys
+from collections import deque
+from itertools import combinations
 input = sys.stdin.readline
 
-from collections import deque
-import copy
-
 N, M = map(int, input().split())
-
 graph = [list(map(int, input().split())) for _ in range(N)]
 
-dy = [0,0,1,-1]
-dx = [1,-1,0,0]
+dy = [0, 0, 1, -1]
+dx = [1, -1, 0, 0]
 
-def bfs():
-    global ans
-    q = deque()
-    temp = copy.deepcopy(graph)
+# 0인 칸, 바이러스 칸 위치 미리 저장
+empty = []
+virus = []
 
-    for i in range(N):
-        for j in range(M):
-            if temp[i][j] == 2:
-                q.append((i,j))
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 0:
+            empty.append((i, j))
+        elif graph[i][j] == 2:
+            virus.append((i, j))
 
+def spread(temp):
+    q = deque(virus)  # 바이러스는 항상 고정된 위치에서 시작
     while q:
         y, x = q.popleft()
-
         for d in range(4):
-            ny, nx = y +dy[d], x + dx[d]
-            if nx<0 or ny<0 or ny>=N or nx >= M: continue
-            if temp[ny][nx] == 0:
+            ny, nx = y + dy[d], x + dx[d]
+            if 0 <= ny < N and 0 <= nx < M and temp[ny][nx] == 0:
                 temp[ny][nx] = 2
-                q.append((ny,nx))
+                q.append((ny, nx))
 
-
-    cnt = 0
-    for i in range(N):
-        cnt += temp[i].count(0)
-
-    ans = max(cnt, ans)
-
-def make_wall(cnt):
-    if cnt == 3:
-        bfs()
-        return
-
-
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 0:
-                graph[i][j] = 1
-                make_wall(cnt+1)
-                graph[i][j] = 0
-
+def get_safe_area(temp):
+    return sum(row.count(0) for row in temp)
 
 ans = 0
-make_wall(0)
+for walls in combinations(empty, 3):
+    temp = [row[:] for row in graph]  # 얕은 복사로도 충분함
+    for y, x in walls:
+        temp[y][x] = 1
+    spread(temp)
+    ans = max(ans, get_safe_area(temp))
+
 print(ans)
