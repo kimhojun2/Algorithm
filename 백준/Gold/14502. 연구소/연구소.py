0d610903ad9620aca244dde73e1877454fc37a75
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-08 21:47:17 +0900

    [Gold IV] Title: 연구소, Time: 2908 ms, Memory: 124472 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,51 +1,57 @@
+import sys
+input = sys.stdin.readline
+
 from collections import deque
 import copy
+
 N, M = map(int, input().split())
 
-arr = [list(map(int, input().split())) for _ in range(N)]
-dy = [1,0,-1,0]
-dx = [0,1,0,-1]
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+dy = [0,0,1,-1]
+dx = [1,-1,0,0]
 
 def bfs():
+    global ans
     q = deque()
-    new_arr = copy.deepcopy(arr)
+    temp = copy.deepcopy(graph)
 
     for i in range(N):
         for j in range(M):
-            if new_arr[i][j] == 2:
+            if temp[i][j] == 2:
                 q.append((i,j))
 
     while q:
         y, x = q.popleft()
 
         for d in range(4):
-            ny, nx = y + dy[d], x + dx[d]
+            ny, nx = y +dy[d], x + dx[d]
+            if nx<0 or ny<0 or ny>=N or nx >= M: continue
+            if temp[ny][nx] == 0:
+                temp[ny][nx] = 2
+                q.append((ny,nx))
 
-            if 0 <= nx < M and 0 <= ny < N:
-                if new_arr[ny][nx] == 0:
-                    new_arr[ny][nx] = 2
-                    q.append((ny, nx))
 
-    global ans
     cnt = 0
     for i in range(N):
-        cnt += new_arr[i].count(0)
+        cnt += temp[i].count(0)
 
-    if cnt > ans:
-        ans = cnt
+    ans = max(cnt, ans)
 
-def wall(cnt):
+def make_wall(cnt):
     if cnt == 3:
         bfs()
         return
 
+
     for i in range(N):
         for j in range(M):
-            if arr[i][j] == 0:
-                arr[i][j] = 1
-                wall(cnt+1)
-                arr[i][j] = 0
+            if graph[i][j] == 0:
+                graph[i][j] = 1
+                make_wall(cnt+1)
+                graph[i][j] = 0
+
 
 ans = 0
-wall(0)
+make_wall(0)
 print(ans)
