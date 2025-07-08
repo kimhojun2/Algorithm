1dd47ee13f1efc7ebcbe93a64dd5cf1ff8351c5e
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-08 17:57:28 +0900

    [Silver II] Title: A → B, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,32 +1,15 @@
-from collections import deque
+def solution(A, B):
+    cnt = 1
+    while B > A:
+        if B % 2 == 0:
+            B //= 2
+        elif str(B)[-1] == '1':
+            B = int(str(B)[:-1])
+        else:
+            return -1
+        cnt += 1
+
+    return cnt if B == A else -1
 
 A, B = map(int, input().split())
-visited = {}
-q = deque()
-q.append(A)
-visited[A] = 0
-
-while q:
-    now = q.popleft()
-
-    if now == B:
-        break
-    for i in range(2):
-        if i == 0:
-            after = 2 * now
-            if after <= B:
-                if after not in visited:
-                    visited[after] = visited[now] + 1
-                    q.append(after)
-
-        else:
-            after = int(str(now) + '1')
-            if after <= B:
-                if after not in visited:
-                    visited[after] = visited[now] + 1
-                    q.append(after)
-
-if B not in visited:
-    print(-1)
-else:
-    print(visited[B]+1)
+print(solution(A, B))
