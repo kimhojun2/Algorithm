aec56c40f4684e0f23b84e1d9fca7b5b55afa48c
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-10 23:53:49 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 5768 ms, Memory: 529392 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -1,36 +1,47 @@
-import sys
-input=sys.stdin.readline
 from collections import deque
-def BFS(x,y,z):
-    dq=deque()
-    dq.append((x,y,z))
-    while dq:
-        x,y,c=dq.popleft()
-        if x == n-1 and y == m-1:
-            return visit[x][y][c]
-        for i in range(4):
-            xx=x+dx[i]
-            yy=y+dy[i]
+import sys
+input = sys.stdin.readline
 
-            if xx<0 or xx>=n or yy < 0 or yy>=m:
-                continue
-            if a[xx][yy] == 1 and c >0 and visit[xx][yy][c-1]==0:
-                visit[xx][yy][c-1] = visit[x][y][c]+1
-                dq.append((xx,yy,c-1))
-            elif a[xx][yy]==0 and visit[xx][yy][c]==0:
-                visit[xx][yy][c]= visit[x][y][c]+1
-                dq.append((xx,yy,c))
-    return -1
-                            
-n,m,k=map(int, input().split())
-a=[list(map(int, input().rstrip())) for _ in range(n)]
+def bfs():
+    q = deque()
+    # (y,x,k)
+    q.append((0,0,0))
+    visited[0][0][0] = 1
+    while q:
+        y,x,k = q.popleft()
 
-visit=[[[0]*(k+1) for _ in range(m)] for _ in range(n)]
-visit[0][0][k]=1
+        if y == N-1 and x == M-1: break
 
 
+        for d in range(4):
+            ny,nx = y+dy[d], x+dx[d]
+            if 0<=ny<N and 0<=nx<M:
+                # 이동이 가능한 공간
+                if graph[ny][nx] == '0' and visited[ny][nx][k] == 0:
+                    visited[ny][nx][k] = visited[y][x][k] + 1
+                    q.append((ny,nx,k))
+                # 이동이 불가능한 공간
+                else:
+                    if k+1 <= K:
+                        if visited[ny][nx][k+1] == 0 and visited[ny][nx][k+1] < visited[y][x][k]:
+                            visited[ny][nx][k+1] = visited[y][x][k] + 1
+                            q.append((ny,nx,k+1))
 
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+    if sum(visited[N-1][M-1]) == 0: return -1
+    else:
+        m = 1000001
+        for k in visited[N-1][M-1]:
+            if k == 0: continue
+            else: m = min(m,k)
+        return m
 
-print(BFS(0,0,k))
+
+N,M,K = map(int, input().split())
+
+graph = [list(input().rstrip()) for _ in range(N)]
+dy = [0,0,1,-1]
+dx = [1,-1,0,0]
+visited = [[[0]*(K+1) for __ in range(M)]for _ in range(N)]
+
+ans = bfs()
+print(ans)
