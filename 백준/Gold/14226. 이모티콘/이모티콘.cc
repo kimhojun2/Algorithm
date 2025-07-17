3a99c5e40eb6e98b9561a59268ec25c439e99c0b
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-07-17 10:43:18 +0900

    [Gold IV] Title: 이모티콘, Time: 12 ms, Memory: 17900 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.cc
+++ 백준/Gold/14226. 이모티콘/이모티콘.cc
@@ -1,57 +1,50 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <algorithm>
 using namespace std;
 
 int S;
-int visited[2001][2001] = { 0 };
+
+void bfs() {
+    vector<vector<int>> visited(2001, vector<int>(2001, -1));
+    queue<pair<int, int>> q;
+
+    q.push({ 1, 0 }); 
+    visited[1][0] = 0;
+
+    while (!q.empty()) {
+        int screen = q.front().first;
+        int clip = q.front().second;
+        q.pop();
+
+        if (screen == S) {
+            cout << visited[screen][clip] << '\n';
+            return;
+        }
+
+        if (visited[screen][screen] == -1) {
+            visited[screen][screen] = visited[screen][clip] + 1;
+            q.push({ screen, screen });
+        }
+
+        if (clip > 0 && screen + clip <= 2000 && visited[screen + clip][clip] == -1) {
+            visited[screen + clip][clip] = visited[screen][clip] + 1;
+            q.push({ screen + clip, clip });
+        }
+
+        if (screen - 1 >= 0 && visited[screen - 1][clip] == -1) {
+            visited[screen - 1][clip] = visited[screen][clip] + 1;
+            q.push({ screen - 1, clip });
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
 
     cin >> S;
-    queue<pair<int, int>> q;
-    q.push({ 1, 0 });
-    visited[1][0] = 1;
+    bfs();
 
-    while (!q.empty()) {
-        int now_screen = q.front().first;
-        int now_clip = q.front().second;
-        q.pop();
-
-        for (int i = 0; i < 3; i++) {
-            int new_screen, new_clip;
-            switch (i) {
-            case 0:
-                new_screen = now_screen;
-                new_clip = now_screen;
-                break;
-            case 1:
-                if (now_clip == 0) continue;
-                new_screen = now_screen + now_clip;
-                new_clip = now_clip;
-                break;
-            case 2:
-                new_screen = now_screen - 1;
-                new_clip = now_clip;
-                break;
-            }
-
-            if (new_screen >= 0 && new_screen <= 2000 && new_clip <= 2000 && visited[new_screen][new_clip] == 0) {
-                visited[new_screen][new_clip] = visited[now_screen][now_clip] + 1;
-                q.push({ new_screen, new_clip });
-            }
-        }
-    }
-
-    int answer = 1e9;
-    for (int i = 0; i <= 2000; i++) {
-        if (visited[S][i] != 0) {
-            answer = min(answer, visited[S][i]);
-        }
-    }
-
-    cout << answer - 1 << '\n';
     return 0;
 }
