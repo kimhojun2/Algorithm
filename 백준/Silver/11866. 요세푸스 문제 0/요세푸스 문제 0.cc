ac3253b634b95ac7f7549d139a397cd71d80f4f1
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-08-07 20:50:48 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    queue<int> q;
+    for (int i = 1; i <= N; i++) {
+        q.push(i);
+    }
+
+    cout << "<";
+    while (!q.empty()) {
+        for (int i = 1; i < K; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+
+        cout << q.front();
+        q.pop();
+
+        if (!q.empty()) cout << ", ";
+    }
+    cout << ">\n";
+
+    return 0;
+}
