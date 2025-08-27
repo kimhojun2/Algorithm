8e7e97a49d48e29a104d6f8ceb1a0767d1bce1d8
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2025-08-27 09:29:55 +0900

    [Silver III] Title: queuestack, Time: 40 ms, Memory: 2648 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int n, m;
+bool flag[100001];
+deque<int> dq;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> flag[i];
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        if (!flag[i]) dq.push_back(x);
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int y; cin >> y;
+        dq.push_front(y);
+        cout << dq.back() << ' ';
+        dq.pop_back();
+    }
+}
