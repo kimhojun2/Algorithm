e2f0eb7f1b25e4af1f6f8477ed37a3b8f204be3d
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2026-01-24 12:38:19 +0900

    [Gold V] Title: 토마토, Time: 88 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,59 +1,73 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <algorithm>
+
+#define MAXSIZE 1001
+
 using namespace std;
 
-int M, N;
+int M, N, area;
+
 int dy[4] = { 0,0,1,-1 };
 int dx[4] = { 1,-1,0,0 };
+
+int arr[MAXSIZE][MAXSIZE];
+int visited[MAXSIZE][MAXSIZE];
+queue<pair<int,int>>q;
+
+
+int bfs() {
+	int L;
+	int days = 0;
+	while (!q.empty()) {
+		L = q.size();
+		for (int i = 0;i < L;i++) {
+			int y = q.front().first;
+			int x = q.front().second;
+			q.pop();
+			for (int d = 0;d < 4;d++) {
+				int ny = y + dy[d];
+				int nx = x + dx[d];
+				if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+				if (visited[ny][nx] || arr[ny][nx] != 0) continue;
+
+				arr[ny][nx] = 1;
+				visited[ny][nx] = 1;
+				area--;
+				q.push({ ny,nx });
+
+			}
+		}
+		days++;
+	}
+
+	if (area == 0) return days-1;
+	return -1;
+
+}
+
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	cin >> M >> N;
-	vector<vector<int>>arr(N, vector<int>(M, 0));
-	vector<vector<int>>visited(N, vector<int>(M, 0));
-	queue<pair<int,int>>q;
+	area = N * M;
+
+	int n;
+
 	for (int i = 0;i < N;i++) {
 		for (int j = 0;j < M;j++) {
-			cin >> arr[i][j];
-			if (arr[i][j] == 1) {
-				visited[i][j] = 1;
-				q.push({ i,j });
-			}
+			cin >> n;
+
+			arr[i][j] = n;
+			if (n == 0) continue;
+			if (n == 1) q.push({ i,j });
+			area--;
 		}
 	}
 
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop();
+	cout << bfs();
 
-		for (int d = 0;d < 4;d++) {
-			int ny = y + dy[d];
-			int nx = x + dx[d];
-			if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-			if (visited[ny][nx] == 0 && arr[ny][nx] == 0) {
-				visited[ny][nx] = visited[y][x] + 1;
-				q.push({ ny,nx });
-			}
-		}
-	}
-	int ans = 0;
-	bool bp = false;
-	for (int i = 0;i < N;i++) {
-		for (int j = 0;j < M;j++) {
-			if (arr[i][j] != -1 and visited[i][j] == 0) {
-				ans = -1;
-				bp = true;
-				break;
-			}
-			ans = max(ans, visited[i][j]-1);
-		}
-		if (bp) break;
-	}
-	cout << ans;
+
 	return 0;
 }
