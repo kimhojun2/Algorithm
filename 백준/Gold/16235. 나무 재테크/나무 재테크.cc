db30f6da6f42ca3916967c2151cceaf14c5d9a64
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2026-02-03 00:09:02 +0900

    [Gold III] Title: 나무 재테크, Time: 216 ms, Memory: 3616 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.cc
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.cc
@@ -1,98 +1,108 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <deque>
+
 using namespace std;
 
 int N, M, K;
-int dy[] = { -1,-1,-1,0,0,1,1,1 };
-int dx[] = { -1,0,1,-1,1,-1,0,1 };
+int energy[10][10];
+int ground[10][10];
 
-vector<int> trees[10][10];
+struct Tree {
+    int age, y, x;
 
-void Cycle(vector<vector<int>>& ground, vector<vector<int>>& add_manure) {
-    vector<int> new_trees[10][10];
-    vector<int> dead_trees[10][10];
+    bool operator<(const Tree& other) const {
+        return age < other.age;
+    }
+};
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (trees[i][j].empty()) continue;
-            sort(trees[i][j].begin(), trees[i][j].end());
-            vector<int> survived;
-            int& manure = ground[i][j];
+int dy[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
+int dx[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
 
-            for (int age : trees[i][j]) {
-                if (manure >= age) {
-                    manure -= age;
-                    survived.push_back(age + 1);
-                }
-                else {
-                    dead_trees[i][j].push_back(age);
-                }
-            }
-            trees[i][j] = survived;
+deque<Tree> trees;
+
+void spring_summer() {
+    deque<Tree> temp;
+    vector<Tree> die;
+
+    for (int i = 0; i < trees.size(); i++) {
+        Tree now = trees[i];
+        if (ground[now.y][now.x] >= now.age) {
+            ground[now.y][now.x] -= now.age;
+            now.age++;
+            temp.push_back(now);
+        }
+        else {
+            die.push_back(now);
         }
     }
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int age : dead_trees[i][j]) {
-                ground[i][j] += (age / 2);
+    for (int i = 0; i < die.size(); i++) {
+        ground[die[i].y][die[i].x] += (die[i].age / 2);
+    }
+    trees = temp;
+}
+
+void autumn() {
+    vector<Tree> baby_trees;
+    for (int i = 0; i < trees.size(); i++) {
+        if (trees[i].age % 5 == 0) {
+            for (int d = 0; d < 8; d++) {
+                int ny = trees[i].y + dy[d];
+                int nx = trees[i].x + dx[d];
+                if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
+                baby_trees.push_back({ 1, ny, nx });
             }
         }
     }
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int age : trees[i][j]) {
-                if (age % 5 != 0) continue;
-                for (int d = 0; d < 8; d++) {
-                    int ny = i + dy[d];
-                    int nx = j + dx[d];
-                    if (ny < 0 || nx < 0 || ny >= N || nx >= N) continue;
-                    new_trees[ny][nx].push_back(1);
-                }
-            }
-        }
+    for (int i = 0; i < baby_trees.size(); i++) {
+        trees.push_front(baby_trees[i]);
     }
+}
 
+void winter() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            trees[i][j].insert(trees[i][j].end(), new_trees[i][j].begin(), new_trees[i][j].end());
-            ground[i][j] += add_manure[i][j];
+            ground[i][j] += energy[i][j];
         }
     }
 }
 
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
 
     cin >> N >> M >> K;
-    vector<vector<int>> ground(N, vector<int>(N, 5));
-    vector<vector<int>> add_manure(N, vector<int>(N, 0));
-
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            cin >> add_manure[i][j];
+            cin >> energy[i][j];
+            ground[i][j] = 5;
         }
     }
 
+    vector<Tree> input;
     for (int i = 0; i < M; i++) {
-        int y, x, age;
-        cin >> y >> x >> age;
-        trees[y - 1][x - 1].push_back(age);
+        int r, c, z;
+        cin >> r >> c >> z;
+        input.push_back({ z, r - 1, c - 1 });
     }
 
-    for (int i = 0; i < K; i++) {
-        Cycle(ground, add_manure);
+    sort(input.begin(), input.end());
+
+    for (int i = 0; i < input.size(); i++) {
+        trees.push_back(input[i]);
     }
 
-    int tree_count = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            tree_count += trees[i][j].size();
-        }
+    while (K--) {
+        spring_summer();
+        autumn();
+        winter();
+        if (trees.empty()) break;
     }
-    cout << tree_count;
+
+    cout << trees.size();
+
     return 0;
 }
