f95e39973c22d69c449eefec8cab9b5a0ffa9537
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2026-02-10 11:21:12 +0900

    [Gold III] Title: 앱, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -1,45 +1,37 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int mem[100]; 
-int cost[100];
+int N, M;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    int N, M;
-    cin >> N >> M;
+	cin >> N >> M;
+	int Max_cost = 0;
+	vector<int> mem(N + 1), cost(N + 1);
+	for (int i = 0;i < N;i++) cin >> mem[i];
+	for (int i = 0;i < N;i++) {
+		cin >> cost[i];
+		Max_cost += cost[i];
+	}
 
-    int Max_Cost = 0;
-    for (int i = 0; i < N; i++) {
-        cin >> mem[i];
-    }
-    for (int i = 0; i < N; i++) {
-        cin >> cost[i];
-        Max_Cost += cost[i];
-    }
+	vector<int>dp(Max_cost + 1, 0);
+	for (int i = 0;i < N;i++) {
+		for (int j = Max_cost;j >= cost[i];j--) {
+			dp[j] = max(dp[j], dp[j - cost[i]] + mem[i]);
+		}
+	}
+	for (int j = 0; j <= Max_cost; j++) {
+		if (dp[j] >= M) {
+			cout << j << '\n';
+			break;
+		}
+	}
 
-    vector<int>dp(Max_Cost+1, 0);
 
-    for (int i = 0; i < N; i++) {
-        int now_mem = mem[i];
-        int now_cost = cost[i];
-
-        for (int j = Max_Cost; j >= now_cost; j--) {
-            dp[j] = max(dp[j], dp[j - now_cost] + now_mem);
-        }
-    }
-
-    for (int j = 0; j <= Max_Cost; j++) {
-        if (dp[j] >= M) {
-            cout << j << '\n';
-            break;
-        }
-    }
-
-    return 0;
+	return 0;
 }
