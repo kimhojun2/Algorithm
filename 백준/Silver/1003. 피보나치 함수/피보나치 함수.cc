c2c5e5535e53a28aad9475421e6a32aa9577e637
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2026-02-19 23:36:30 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,27 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int T, N;
+
+long long dp[41][2];
+
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-	int T;
-	cin >> T;
+	dp[0][0] = 1; dp[0][1] = 0;
+	dp[1][0] = 0; dp[1][1] = 1;
 
-	int dp[41][2] = { 0, };
-	dp[0][0] = 1;
-	dp[1][1] = 1;
-	for (int i = 2;i <= 40;i++) {
+	for (int i = 2; i <= 40; i++) {
 		dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
 		dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
 	}
 
-
-	for (int tc = 0;tc < T;tc++) {
-		int N;
+	cin >> T;
+	while (T--) {
 		cin >> N;
-		cout << dp[N][0] << ' ' << dp[N][1] << '\n';
-
+		cout << dp[N][0] << " " << dp[N][1] << "\n";
 	}
 
 	return 0;
