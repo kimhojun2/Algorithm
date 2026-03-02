0b2c7e528829a23d612ebbe6f8cb845d770ff0cb
Kim Ho-Jun <139518050+kimhojun2@users.noreply.github.com>
2026-03-02 21:35:58 +0900

    [Gold II] Title: 확장 게임, Time: 24 ms, Memory: 10584 KB -BaekjoonHub

--- 백준/Gold/16920. 확장 게임/확장 게임.cc
+++ 백준/Gold/16920. 확장 게임/확장 게임.cc
@@ -1,52 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <string>
+#include <queue>
 using namespace std;
 
 int N, M, P;
-int dy[4] = { 0, 0, 1, -1 };
-int dx[4] = { 1, -1, 0, 0 };
-vector<int> S;
-vector<string> arr;
-vector<queue<pair<int, int>>> q;
+int spd[10];
+char arr[1000][1000];
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int dy[] = { 0,0,1,-1 };
+int dx[] = { 1,-1,0,0 };
 
-    cin >> N >> M >> P;
-    S.resize(P + 1);
-    q.resize(P + 1);
+queue<pair<int, int>> q[10];
+int cnt[10];
 
-    for (int i = 1; i <= P; i++) cin >> S[i];
-
-    arr.resize(N);
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
-        for (int j = 0; j < M; j++) {
-            if (arr[i][j] >= '1' && arr[i][j] <= '9') {
-                int p = arr[i][j] - '0';
-                q[p].push(make_pair(i, j));
-            }
-        }
-    }
-
-    while (true) {
+void bfs() {
+    while (1) {
         bool moved = false;
 
-        for (int p = 1; p <= P; p++) {
-            int speed = S[p];
-            queue<pair<int, int>> tmp_q = q[p];
-            q[p] = queue<pair<int, int>>();
+        for (int i = 1; i <= P; i++) {
+            for (int t = 0; t < spd[i]; t++) {
+                int qs = (int)q[i].size();
+                if (qs == 0) break;
 
-            for (int s = 0; s < speed && !tmp_q.empty(); s++) {
-                int sz = tmp_q.size();
-                for (int i = 0; i < sz; i++) {
-                    pair<int, int> cur = tmp_q.front();
-                    tmp_q.pop();
-                    int y = cur.first;
-                    int x = cur.second;
+                while (qs--) {
+                    int y = q[i].front().first;
+                    int x = q[i].front().second;
+                    q[i].pop();
 
                     for (int d = 0; d < 4; d++) {
                         int ny = y + dy[d];
@@ -54,9 +34,9 @@ int main() {
                         if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
                         if (arr[ny][nx] != '.') continue;
 
-                        arr[ny][nx] = p + '0';
-                        q[p].push(make_pair(ny, nx));
-                        tmp_q.push(make_pair(ny, nx));
+                        arr[ny][nx] = char('0' + i);
+                        q[i].push({ ny,nx });
+                        cnt[i]++;
                         moved = true;
                     }
                 }
@@ -65,12 +45,32 @@ int main() {
 
         if (!moved) break;
     }
+}
 
-    vector<int> result(P + 1, 0);
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < M; j++)
-            if (arr[i][j] >= '1' && arr[i][j] <= '9')
-                result[arr[i][j] - '0']++;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    for (int i = 1; i <= P; i++) cout << result[i] << " ";
+    cin >> N >> M >> P;
+    for (int i = 1; i <= P; i++) cin >> spd[i];
+
+    for (int i = 0; i < N; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < M; j++) {
+            arr[i][j] = s[j];
+            if (arr[i][j] >= '1' && arr[i][j] <= '9') {
+                int idx = arr[i][j] - '0';
+                q[idx].push({ i,j });
+                cnt[idx]++;
+            }
+        }
+    }
+
+    bfs();
+
+    for (int i = 1; i <= P; i++) {
+        cout << cnt[i] <<" ";
+    }
+    return 0;
 }
